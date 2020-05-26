def42510a5941c765295989162e1fd2ba0fb6ff8
mounika <mounikasantoshi99@gail.com>
2020-05-26 15:43:06 +0530

    initial commit

---
+++ members.js
@@ -0,0 +1,21 @@
+const members = [
+  {
+    id: 1,
+    name: 'mounika',
+    email: 'mounika@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'santoshi',
+    email: 'santoshi@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 3,
+    name: 'mouni',
+    email: 'mouni@gmail.com',
+    status: 'active',
+  },
+];
+module.exports = members;
